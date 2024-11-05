0161ef25387ee2c41e903323869a5d59073ec84d
s0wime <pokhodun.vitalii@lll.kpi.ua>
2024-11-05 23:14:49 +0200

    work with data types in fundamentals

--- Fundamentals/script.js
+++ Fundamentals/script.js
@@ -1,1 +1,24 @@
-alert("Hello World!");
+let firstName = "Jonas";
+
+console.log(firstName);
+
+// HOW TO USE COMMENTS
+
+let booleanValue = true;
+console.log(booleanValue);
+console.log(typeof booleanValue);
+console.log(typeof 23);
+console.log(typeof "string");
+
+booleanValue = "make boolean as string";
+console.log(booleanValue);
+console.log(typeof booleanValue);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2121;
+console.log(typeof year);
+
+console.log(typeof null);
