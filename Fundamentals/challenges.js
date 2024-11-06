804d5fe9c10ad91779562521c1df9e51db57359f
s0wime <pokhodun.vitalii@lll.kpi.ua>
2024-11-06 21:09:41 +0200

    solve challenge N3 in fundamentals

--- Fundamentals/challenges.js
+++ Fundamentals/challenges.js
@@ -26,4 +26,18 @@ if (markHigherBMI === true) {
 );
 }
 
+
+// CHALLENGE 3
+
+const scoreDolphins = (96 + 108 + 89) / 3; 
+const scoreKoalas = (88 + 91 + 110) / 3;
+
+if (scoreDolphins > scoreKoalas) {
+    console.log("Dolphins win the trophy");
+} else if (scoreKoalas > scoreDolphins) {
+    console.log("Koalas win the trophy");
+} else {
+    console.log("Both win the trophy");
+}
+
 */
