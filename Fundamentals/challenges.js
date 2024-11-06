266a507ac5d07c80c4276144ddfb436820aedbe1
s0wime <pokhodun.vitalii@lll.kpi.ua>
2024-11-06 10:44:50 +0200

    solve first two challenges in fundamentals

---
+++ Fundamentals/challenges.js
@@ -0,0 +1,29 @@
+/* 
+
+// CHALLENGE 1
+
+const weightMark = 78;
+const heightMark = 1.69;
+
+const weightJohn = 92;
+const heightJohn = 1.95;
+
+const markBMI = weightMark / heightMark ** 2;
+const johnBMI = weightJohn / heightJohn ** 2;
+
+const markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
+
+// CHALLENGE 2
+
+if (markHigherBMI === true) {
+  console.log(
+    "Mark's BMI (" + markBMI + ") is higher than John's (" + johnBMI + ")!"
+  );
+} else {
+  console.log(
+"John's BMI (" + johnBMI + ") is higher than Mark's (" + markBMI + ")!"
+);
+}
+
+*/
