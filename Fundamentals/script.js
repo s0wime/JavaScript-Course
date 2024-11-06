c577b0cf030e0e1750a7d050053011063102bdfe
s0wime <pokhodun.vitalii@lll.kpi.ua>
2024-11-06 10:30:31 +0200

    work with operators in fundamentals

--- Fundamentals/script.js
+++ Fundamentals/script.js
@@ -49,4 +49,30 @@ console.log(job);
 
 /* WORK WITH OPERATORS
 
+const now = 2041;
+const ageJonas = now - 2002;
+const ageSarah = now - 1999;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageSarah / 10, 2 ** 3);
+
+const firstName = "Name";
+const lastName = "Last";
+console.log(firstName + " " + lastName);
+
+let x = 10 + 5;
+console.log(x);
+x += 10;
+console.log(x);
+x *= 10;
+x++;
+x--;
+console.log(x);
+
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageJonas >= 18;
+console.log(isFullAge);
+
 */
