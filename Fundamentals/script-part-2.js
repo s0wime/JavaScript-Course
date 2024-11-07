d2b12156ab1a70db5f0c039f9d40da4f4c61cc5f
s0wime <pokhodun.vitalii@lll.kpi.ua>
2024-11-07 12:34:47 +0200

    work with function declarations, expressions and arrow functions in fundamentals

--- Fundamentals/script-part-2.js
+++ Fundamentals/script-part-2.js
@@ -19,3 +19,38 @@ console.log(fruitProccessor(3, 5));
 console.log(fruitProccessor(11, 8));
 
 */
+
+/* WORK WITH FUNCTION DECLARATIONS, EXPRESSIONS AND ARROW FUNCTIONS
+
+// FUNCTION DECLARATION
+
+function calcAge1(birthYear) {
+  return 2047 - birthYear;
+}
+
+const age1 = calcAge1(2000);
+console.log(`Your age is : ${age1}`);
+
+// FUNCTION EXPRESSION
+
+const calcAge2 = function (birthYear) {
+  return 2047 - birthYear;
+};
+const age2 = calcAge2(1999);
+console.log(`Your age is : ${age2}`);
+
+// ARROW FUNCTION
+
+const calcAge3 = (birthYear) => 2047 - birthYear;
+const age3 = calcAge2(1998);
+console.log(`Your age is : ${age3}`);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2047 - birthYear;
+  const retirementAge = 65;
+  return `${firstName} retires in ${retirementAge - age} years`;
+};
+console.log(yearsUntilRetirement(1997, "Jonas"));
+console.log(yearsUntilRetirement(1994, "Bob"));
+
+*/
