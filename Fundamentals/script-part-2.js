7966b95b067c755066d88cedd7a59a0c8c53610c
s0wime <pokhodun.vitalii@lll.kpi.ua>
2024-11-07 12:12:14 +0200

    work with functions in fundamentals

--- Fundamentals/script-part-2.js
+++ Fundamentals/script-part-2.js
@@ -1,1 +1,21 @@
 "use strict";
+
+/* WORK WITH FUNCTIONS
+
+function logger() {
+  console.log("Helllooo!");
+}
+
+logger();
+logger();
+logger();
+
+function fruitProccessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges!`;
+  return juice;
+}
+
+console.log(fruitProccessor(3, 5));
+console.log(fruitProccessor(11, 8));
+
+*/
