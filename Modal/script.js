6f9f567d524b1ba84b336c944ce4b70e7812fd80
s0wime <pokhodun.vitalii@lll.kpi.ua>
2024-11-11 18:18:22 +0200

    handle key pressing in modal

--- Modal/script.js
+++ Modal/script.js
@@ -21,3 +21,11 @@ for (let i = 0; i < btnsShowModal.length; i++) {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
